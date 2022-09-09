31a5c8e233dfe6bc91ac490af9bdd2bb50a1d67b
TjulienP <107543248+TjulienP@users.noreply.github.com>
2022-09-09 09:56:45 +0200

    fix:url patient service front
    Correction des urls dans le front pour les patients. p minuscule au lieu de P majuscule

--- src/app/service/service-patient.service.ts
+++ src/app/service/service-patient.service.ts
@@ -14,24 +14,24 @@ export class ServicePatientService {
 
 
   public getAllPatient = (): Observable<Patient[]> => {
-    return this.http.get<Patient[]>(`${this.domaine}/Patient`)
+    return this.http.get<Patient[]>(`${this.domaine}/patient`)
   }
 
   getPatientById = (id: string) => {
-    return this.http.get(`${this.domaine}/Patient/${id}`)
+    return this.http.get(`${this.domaine}/patient/${id}`)
   }
   getPatientByName = (name: string): Observable<Patient[]>  => {
-    return this.http.get<Patient[]>(`${this.domaine}/Patient/name/${name}`)
+    return this.http.get<Patient[]>(`${this.domaine}/patient/name/${name}`)
   }
 
   deletePatient = (id: string | undefined) => {
-    return this.http.delete(`${this.domaine}/Patient/${id}`)
+    return this.http.delete(`${this.domaine}/patient/${id}`)
   }
 
   postPatient: (item: Patient) => Observable<Patient> = (item: Patient) => {
-    return this.http.post<Patient>(`${this.domaine}/Patient/post`, item)
+    return this.http.post<Patient>(`${this.domaine}/patient/`, item)
   }
   updatePatient:(item: Patient, id: string | undefined ) => Observable<Patient> = (item: Patient, id:string | undefined) => {
-    return this.http.put(`${this.domaine}/Patient/${id}`, item)
+    return this.http.put(`${this.domaine}/patient/${id}`, item)
   }
 }
